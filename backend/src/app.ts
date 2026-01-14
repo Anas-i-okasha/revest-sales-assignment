96a2bebcd1236f1055cfa9658f66987959f8a0ff
Anas okasha <aokasha@smartchargetech.com>
2026-01-14 17:50:56 +0300

    start-integrate-with-frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+// import { join } from 'path';
 import authRouter from './routes/auth.routes';
 import productRouter from './routes/product.route';
 import SalesOrderRoute from './routes/salesOrder.route';
@@ -14,4 +15,13 @@ app.use('/api/products', productRouter);
 app.use('/api/sales-order', SalesOrderRoute);
 
 /**********************  */
+
+// const angularDistPath = join(__dirname, '../../frontend/dist/frontend');
+
+// app.use(express.static(angularDistPath));
+
+// app.get(/^\/(?!api).*/, (req, res) => {
+// 	res.sendFile(join(angularDistPath, 'index.html'));
+// });
+
 export default app;
